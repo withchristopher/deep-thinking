0d3541cdb92dbb9a2987a90c14a6c065be6e56ca
Chris Maxwell <chmaxwel@icloud.com>
2020-10-20 18:04:22 +0200

    ++profile page

--- client/src/pages/SingleThought.js
+++ client/src/pages/SingleThought.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/react-hooks';
 import { QUERY_THOUGHT } from '../utils/queries';
+import ReactionList from '../components/ReactionList';
 
 const SingleThought = props => {
   const { id: thoughtId } = useParams();
@@ -29,6 +30,8 @@ const SingleThought = props => {
           <p>{thought.thoughtText}</p>
         </div>
       </div>
+
+      {thought.reactionCount > 0 && <ReactionList reactions={thought.reactions} />}
     </div>
   );
 };
